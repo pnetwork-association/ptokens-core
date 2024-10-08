d2f3507d552da115a981d2c0b0a8f0d0ae35451a
Ulisse Bordignon <ulisse.bordignon@protonmail.com>
2024-10-08 11:37:27 +0200

    fix(v2-int-on-eos): reflect `spring_1-0` gate on crate

--- v2_bridges/int_on_eos/src/eos/get_eos_output.rs
+++ v2_bridges/int_on_eos/src/eos/get_eos_output.rs
@@ -85,6 +85,9 @@ pub fn get_eos_output<D: DatabaseInterface>(state: EosState<D>) -> Result<String
     info!("✔ Getting EOS output json...");
     let int_signed_txs = state.eth_signed_txs;
     let output = serde_json::to_string(&EosOutput {
+        #[cfg(feature = "spring_1-0")]
+        eos_latest_block_number: state.eos_db_utils.get_latest_eos_block_number()?,
+        #[cfg(not(feature = "spring_1-0"))]
         eos_latest_block_number: state.incremerkles.latest_block_num(),
         int_signed_transactions: if int_signed_txs.is_empty() {
             vec![]
